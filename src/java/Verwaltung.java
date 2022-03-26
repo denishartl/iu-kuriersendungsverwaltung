a310d6e9477489facb66e6b0571e8ba3204cb5d7
denishartl <denis.hartl@icloud.com>
2022-03-26 16:59:49 +0100

    Working version with ManagedBean in Verwaltung

--- src/java/Verwaltung.java
+++ src/java/Verwaltung.java
@@ -3,17 +3,6 @@ import java.util.List;
 import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 
-
-
-
-
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author hartlden
